0beebea6a060d8dbb20e265fda5f6ad48f3d6661
Daniel Ranger <dranger003@gmail.com>
2014-03-13 18:22:29 +0000

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Web.Hosting;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing ASP.NET runtime...");
+            var host = (WebHost.WebHost)ApplicationHost.CreateApplicationHost(
+                typeof(WebHost.WebHost),
+                "/",
+                Directory.GetCurrentDirectory());
+            Console.WriteLine("Done.");
+
+            Console.WriteLine("Initializing HTTP listener...");
+            var server = new HttpListener();
+            server.Prefixes.Add("http://192.168.2.24:12345/");
+            server.Start();
+            Console.WriteLine("Done.");
+
+            var query = "";
+            while (query != "q=1")
+            {
+                var context = server.GetContext();
+
+                using (var response = context.Response)
+                {
+                    using (var writer = new StreamWriter(response.OutputStream))
+                    {
+                        var page = context.Request.Url.LocalPath.Substring(1);
+                        if (String.IsNullOrWhiteSpace(page))
+                            page = "Default.cshtml";
+
+                        query = context.Request.Url.Query.Replace("?", "");
+
+                        host.ProcessRequest(page, query, writer);
+                        Console.WriteLine("[{0}][{1}]", page, query);
+                    }
+                }
+            }
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("\nPress <any key> to continue.");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ WebHost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+//using System.Web.Routing;
+
+//[assembly: PreApplicationStartMethod(typeof(WebHost.RouteInitializer), "Initialize")]
+
+namespace WebHost
+{
+    public class WebHost : MarshalByRefObject
+    {
+        public void ProcessRequest(string page, string query, StreamWriter output)
+        {
+            HttpRuntime.ProcessRequest(new SimpleWorkerRequest(page, query, output));
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            // This tells the CLR not to surreptitiously 
+            // destroy this object -- it's a singleton
+            // and will live for the life of the appdomain
+            return null;
+        }
+    }
+}
